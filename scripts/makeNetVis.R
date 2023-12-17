71eb9780de0f543b01cda2ff43b2dabc18109728
RowanRampton <rowan.rampton@ucalgary.ca>
2023-12-17 15:59:20 -0700

    u

--- scripts/makeNetVis.R
+++ scripts/makeNetVis.R
@@ -1,11 +1,29 @@
 source("scripts/intersectShapes.R")
-
+library(pdftools)
 library(tidyverse)
 library(bipartiteD3)
 library(r2d3)
 library(bipartite)
 library(RColorBrewer)
 
+library(magick)
+library(patchwork)
+library(rsvg)
+
+#library(r2d3svg)
+#need to install r2d3svg: remotes::install_github("usda-nifa-b-team/r2d3svg)
+# i had to remove htmltools from appdata/local/r/winlibrary to get it to install
+#remotes::install_github("usda-nifa-b-team/r2d3svg")
+
+toBipd3 <- function(areaSubset, pol = pollinatorINatName, plnt = plantINatName){
+  d3Dat <- areaSubset %>% 
+    as_tibble() %>% 
+    group_by({{pol}},{{plnt}}) %>% 
+    summarise(n = n()) %>% 
+    as_tibble() %>% 
+    dplyr::select({{pol}},{{plnt}},n)
+  return(d3Dat)
+}
 # fixing some names
 filtSubset <- plant.poll.sf.labels %>% 
   filter(!grepl("\"", plantINatName)) %>% 
@@ -28,11 +46,8 @@ steensExample <- filtSubset %>%
 
 d3ready <- toBipd3(areaSubset = steensExample)
 
-#modded functions from bipartite_D3
-rm(bipartiteD3)
-library(bipartiteD3)
 netVis <- bipartite_D3(d3ready,
-             MainFigSize = c(2000,1760),
+             MainFigSize = c(990,1760),
              IndivFigSize = c(400,1550),
              PercPos = c(340,450),
              Pad = 2,
@@ -41,16 +56,13 @@ netVis <- bipartite_D3(d3ready,
              PrimaryLab = "Bees",
              SecondaryLab = "Plants"
 )
-#save_d3_html(netVis, "steensTest.html")
+#save_d3_html(netVis, "testVis.html")
+source("r2d3svg/R/save_d3_pdf.R")
+source("r2d3svg/R/save_d3_svg.R")
+wid <- 1200
+hgt <- 1760
+save_d3_svg(netVis, "visFile2.svg", width = wid, height = hgt)
+#debugonce(save_d3_svg)
+#need to recode save d3 svg to open with a window 10% bigger than the visualization
 
-library(r2d3svg)
-r2d3svg::save_d3_svg(netVis, "visFile.svg")
-
-library(magick)
-library(patchwork)
-library(rsvg)
-
-test <- image_read_svg("visFile.svg")
-
-rsvg::rsvg_pdf("visFile.svg", file = "testSvg.pdf", css = "bipartiteD3Script.css")
-
+rsvg::rsvg_pdf("visFile2.svg", file = "testSvg2.pdf", css = "bipartiteD3Script.css")
